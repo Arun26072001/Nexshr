484aa5f2d550a79a0a9edd7c58ee32446c814363
Arunkumar AD <arun@webnexs.in>
2024-12-24 13:45:24 +0530

    updated

--- nexr_frontend_org/src/components/Login.jsx
+++ nexr_frontend_org/src/components/Login.jsx
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
-import Logo from "../imgs/webnexs_logo.webp";
 import { ScaleLoader } from "react-spinners";
 import { EssentialValues } from "../App";
 import "./Login.css";
+import Logo from "../imgs/webnexs_logo.webp";
 
 const Login = () => {
   const { handleSubmit, loading, pass } = useContext(EssentialValues);
@@ -10,20 +10,21 @@ const Login = () => {
   return (
     <div className="container">
       <div id="main-outer-div">
+        {/* If you decide to include the logo later, uncomment the following div */}
         {/* <div id="logo-div">
           <img
             id="logo-img"
             src={Logo}
-            // loading="eager"
             width={100}
             height={100}
-            style={{ objectFit: "cover" }}
             alt="Webnexs Logo"
+            style={{ objectFit: "cover" }}
           />
         </div> */}
-        <div id="title-div">
-          <span className="title">Sign in</span>
-        </div>
+
+
+        <p className="title">Sign in</p>
+
         <div id="outer-login-form-div">
           <form onSubmit={handleSubmit}>
             <input
@@ -46,14 +47,12 @@ const Login = () => {
               value="Sign in"
               id="submitBtn"
             />
-            {!pass && (
-              <p className="alert">Invalid Username or Password</p>
-            )}
+            {!pass && <p className="alert">Invalid Username or Password</p>}
           </form>
         </div>
         {loading && (
           <div className="loading">
-            <ScaleLoader size={150} color={"#123abc"} />
+            <ScaleLoader size={150} color="#123abc" />
           </div>
         )}
       </div>
